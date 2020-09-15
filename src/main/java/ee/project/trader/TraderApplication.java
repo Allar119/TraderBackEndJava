22cddff1ee7ae91771b74199b5c4b00c195190f7
Allar <allar.ounsaar@gmail.com>
2020-09-15 09:53:15 +0300

    Initial Commit

---
+++ src/main/java/ee/project/trader/TraderApplication.java
@@ -0,0 +1,16 @@
+package ee.project.trader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderApplication.class, args);
+
+		System.out.println("test");
+
+	}
+
+}
